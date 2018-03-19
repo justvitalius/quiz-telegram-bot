db9d534177174e7cfb9d64a1f25293c465c1847e
justvitalius <justvitalius@gmail.com>
2018-03-19 22:13:24 +0300

    Совмещен бот и подбиратель вопросов

--- questionaries.js
+++ questionaries.js
@@ -1,52 +1,33 @@
 module.exports = [
   {
-    question: 'Каких операторов из этого списка нет в javascript?',
-    options: [
-      '*',
-      '^',
-      '%',
-      '#',
-      '&',
-      '>>',
-      '>>>',
-      '!'
-    ],
+    question: "Каких операторов из этого списка нет в javascript?",
+    type: 0,
+    options: ["*", "^", "%", "#", "&", ">>", ">>>", "!"],
     answer: {
       compareByValue: true,
-      value: '#'
+      value: "#"
     }
   },
   {
     question: `
-    Что выведет этот код
-      let a = (1,5 - 1) * 2;
-      alert(a);
+Что выведет этот код?
+let a = (1,5 - 1) * 2;
+alert(a);
     `,
-    options: [
-      '0.9999999',
-      '1',
-      '0.5',
-      '8',
-      '-0.5',
-      '4'
-    ],
+    type: 0,
+    options: ["0.9999999", "1", "0.5", "8", "-0.5", "4"],
     answer: {
       compareByIndex: true,
       value: 3
     }
   },
   {
-    question: 'Чему равна сумма [] + 1 + 2?',
-    options: [
-      '1',
-      'NaN',
-      'undefined',
-      '12',
-      'Другое'
-    ],
+    question: "Чему равна сумма [] + 1 + 2?",
+    type: 0,
+    options: ["1", "NaN", "undefined", "12", "Другое"],
     answer: {
       compareByExpression: true,
-      value: '12'
+      value: "12"
     }
   }
 ];
