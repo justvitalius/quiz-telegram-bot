cd91618e8106bbe3d3c541da9dd75d8d4eb0d15c
w3dip <w3dipmail@gmail.com>
2018-05-12 22:49:50 +0300

    12_05_bugs: Добавлена реализация api для статистики из непринятого ранее ПР (поэтому категории и не показывались в статистике)

--- src/statistics/endpoints.js
+++ src/statistics/endpoints.js
@@ -1,4 +1,4 @@
-const { getAllUsers } = require("../database");
+const { getAllUsers, getAllCategories } = require("../database");
 
 module.exports = function(app) {
   app.all("/*", function(req, res, next) {
@@ -7,9 +7,8 @@ module.exports = function(app) {
     next();
   });
 
-  //TODO implement
   app.route("/categories").get((req, res) => {
-    res.send([{}]);
+    getAllCategories().then(categories => res.send(categories));
   });
 
   //TODO implement
