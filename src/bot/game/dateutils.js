34c1010720742bcaf8ec44d01114ecdee7db3564
justvitalius <justvitalius@gmail.com>
2018-05-16 19:13:40 +0300

    Добавлено чтение времени начала/конца из конфиг.файла

--- src/bot/game/dateutils.js
+++ src/bot/game/dateutils.js
@@ -1,10 +1,13 @@
 const moment = require("moment");
 const timeFormat = "hh:mm";
+const config = require("config");
+const openTime = config.get("bot.openTime");
+const closeTime = config.get("bot.closeTime");
 
 function isTestAvailableByTime() {
   const timeNow = moment();
-  const timeStartTest = moment("09:00", timeFormat);
-  const timeEndTest = moment("19:00", timeFormat);
+  const timeStartTest = moment(openTime, timeFormat);
+  const timeEndTest = moment(closeTime, timeFormat);
   return timeNow.isBetween(timeStartTest, timeEndTest);
 }
 
